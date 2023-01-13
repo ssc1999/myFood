106eda73660b6d4bc29e127af83985adda867507
ssc1099 <ssc1099.csmm@gmail.com>
2023-01-14 00:53:46 +0100

    ✨ feat(api-gateway): food extracted from ext API

--- myFood/backend/api_gateway/services/foodService.js
+++ myFood/backend/api_gateway/services/foodService.js
@@ -5,49 +5,92 @@ const apiAdapter = require("../routers/apiAdapter");
 const paths = require("./paths");
 const BASE_URL = "http://127.0.0.1:8000";
 const api = apiAdapter(BASE_URL);
+const externalApi = apiAdapter();
+const axios = require("axios");
 
-// get all food
-router.get("/food", (routerRequest, routerResponse) => {
-    api.get(routerRequest.path).then((fastApiResponse) => {
-        routerResponse.send(fastApiResponse.data);
-    });
-});
 
 // get food by name
-router.get("/food/search/**", (routerRequest, routerResponse) => {
-    var query = JSON.stringify(routerRequest.query);
-    api.get(routerRequest.path, { params: routerRequest.query }).then(
-        (fastApiResponse) => {
-            routerResponse.send(fastApiResponse.data);
+router.get("/api/food/search/**", (routerRequest, routerResponse) => {
+    console.log({routerRequest})
+    const options = {
+        method: 'GET',
+        url: 'https://edamam-food-and-grocery-database.p.rapidapi.com/parser',
+        params: routerRequest.query,
+        headers: {
+            'X-RapidAPI-Key': 'e6b6f6c0a5mshb1264d10e679cffp1ec677jsncac5cfff3a61',
+            'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
         }
-    );
+    };
+      
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+        routerResponse.send(response.data);
+    }).catch(function (error) {
+        routerResponse.send(error);
+    });
 });
 
 // get a food by id
-router.get("/food/*", (routerRequest, routerResponse) => {
-    console.log(routerRequest.path);
-    api.get(routerRequest.path).then((fastApiResponse) => {
-        console.log(fastApiResponse.data);
-        routerResponse.send(fastApiResponse.data);
+router.get("/api/food/**", (routerRequest, routerResponse) => {
+    console.log({routerRequest})
+    const options = {
+        method: 'GET',
+        url: 'https://edamam-food-and-grocery-database.p.rapidapi.com/parser',
+        params: routerRequest.query,
+        headers: {
+            'X-RapidAPI-Key': 'e6b6f6c0a5mshb1264d10e679cffp1ec677jsncac5cfff3a61',
+            'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
+        }
+    };
+      
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+        routerResponse.send(response.data);
+    }).catch(function (error) {
+        routerResponse.send(error);
     });
 });
 
-// post a food
-router.post("/food", (routerRequest, routerResponse) => {
-    console.log(routerRequest.body);
-    api.post(routerRequest.path, routerRequest.body).then((fastApiResponse) => {
-        console.log(fastApiResponse.data);
-        routerResponse.send(fastApiResponse.data);
-    });
-});
+// // get all food
+// router.get("/food", (routerRequest, routerResponse) => {
+//     api.get(routerRequest.path).then((fastApiResponse) => {
+//         routerResponse.send(fastApiResponse.data);
+//     });
+// });
 
-// delete a food by id
-router.delete("/food/*", (routerRequest, routerResponse) => {
-    console.log(routerRequest.path);
-    api.get(routerRequest.path).then((fastApiResponse) => {
-        console.log(fastApiResponse.data);
-        routerResponse.send(fastApiResponse.data);
-    });
-});
+// // post a food
+// router.post("/food", (routerRequest, routerResponse) => {
+//     console.log(routerRequest.body);
+//     api.post(routerRequest.path, routerRequest.body).then((fastApiResponse) => {
+//         console.log(fastApiResponse.data);
+//         routerResponse.send(fastApiResponse.data);
+//     });
+// });
+
+// // delete a food by id
+// router.delete("/food/*", (routerRequest, routerResponse) => {
+//     console.log(routerRequest.path);
+//     api.get(routerRequest.path).then((fastApiResponse) => {
+//         console.log(fastApiResponse.data);
+//         routerResponse.send(fastApiResponse.data);
+//     });
+// });
+
+// router.get("/food/search/**", (routerRequest, routerResponse) => {
+//     var query = JSON.stringify(routerRequest.query);
+//     api.get(routerRequest.path, { params: routerRequest.query }).then(
+//         (fastApiResponse) => {
+//             routerResponse.send(fastApiResponse.data);
+//         }
+//     );
+// });
+
+// router.get("/food/*", (routerRequest, routerResponse) => {
+//     console.log(routerRequest.path);
+//     api.get(routerRequest.path).then((fastApiResponse) => {
+//         console.log(fastApiResponse.data);
+//         routerResponse.send(fastApiResponse.data);
+//     });
+// });
 
 module.exports = router;
